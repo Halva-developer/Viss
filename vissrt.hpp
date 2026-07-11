a76c3904dc71f2f868ae62fc05e64059717e0b94
halvik <halvik@viss.dev>
2026-07-11 17:29:47 +0400

    Enhance WebViss library with startGateway and registerProtocol to support native browser routing of vts:// scheme requests through local HTTP proxy gateway.

--- vissrt.hpp
+++ vissrt.hpp
@@ -500,6 +500,100 @@ namespace viss {
             }
             return "[WebViss Error] Unsupported protocol in URL: " + url;
         }
+
+        inline void registerProtocol() {
+            #ifdef _WIN32
+            HKEY hKey;
+            char path[MAX_PATH];
+            GetModuleFileNameA(nullptr, path, MAX_PATH);
+            std::string appPath(path);
+
+            if (RegCreateKeyExA(HKEY_CLASSES_ROOT, "vts", 0, nullptr, REG_OPTION_NON_VOLATILE, KEY_WRITE, nullptr, &hKey, nullptr) == ERROR_SUCCESS) {
+                std::string desc = "URL:Viss Tunnel Security Protocol";
+                RegSetValueExA(hKey, nullptr, 0, REG_SZ, (const BYTE*)desc.c_str(), (DWORD)(desc.length() + 1));
+                std::string urlProto = "";
+                RegSetValueExA(hKey, "URL Protocol", 0, REG_SZ, (const BYTE*)urlProto.c_str(), 1);
+                RegCloseKey(hKey);
+            }
+
+            if (RegCreateKeyExA(HKEY_CLASSES_ROOT, "vts\\shell\\open\\command", 0, nullptr, REG_OPTION_NON_VOLATILE, KEY_WRITE, nullptr, &hKey, nullptr) == ERROR_SUCCESS) {
+                std::string cmd = "\"" + appPath + "\" \"%1\"";
+                RegSetValueExA(hKey, nullptr, 0, REG_SZ, (const BYTE*)cmd.c_str(), (DWORD)(cmd.length() + 1));
+                RegCloseKey(hKey);
+            }
+            std::cout << "[WebViss] VTS protocol successfully registered in Windows Registry.\n";
+            std::cout << "[WebViss] Typing 'vts://<site>' in Run or browser will launch this gateway!\n";
+            #else
+            std::cout << "[WebViss] URI scheme registration is only supported on Windows.\n";
+            #endif
+        }
+
+        inline void startGateway(Int port) {
+            std::cout << "[WebViss Gateway] Starting HTTP-to-VTS Gateway on http://localhost:" << port << "\n";
+            std::thread([port]() {
+                #ifdef _WIN32
+                WSADATA wsa;
+                WSAStartup(MAKEWORD(2,2), &wsa);
+                #endif
+
+                SOCKET server = socket(AF_INET, SOCK_STREAM, 0);
+                sockaddr_in addr = {};
+                addr.sin_family = AF_INET;
+                addr.sin_port = htons(port);
+                addr.sin_addr.s_addr = INADDR_ANY;
+
+                int opt = 1;
+                setsockopt(server, SOL_SOCKET, SO_REUSEADDR, (const char*)&opt, sizeof(opt));
+
+                if (bind(server, (sockaddr*)&addr, sizeof(addr)) == SOCKET_ERROR) {
+                    std::cout << "[WebViss Gateway] Error binding to port " << port << "\n";
+                    return;
+                }
+
+                listen(server, 10);
+
+                while (true) {
+                    SOCKET client = accept(server, nullptr, nullptr);
+                    if (client == INVALID_SOCKET) continue;
+
+                    char buf[2048] = {0};
+                    int bytes = recv(client, buf, sizeof(buf) - 1, 0);
+                    if (bytes > 0) {
+                        std::string req(buf);
+                        size_t fetchPos = req.find("/fetch?url=");
+                        if (fetchPos != std::string::npos) {
+                            size_t urlStart = fetchPos + 11;
+                            size_t urlEnd = req.find(" ", urlStart);
+                            std::string targetUrl = req.substr(urlStart, urlEnd - urlStart);
+                            
+                            // URL decode helper
+                            auto replaceAll = [](std::string& s, const std::string& f, const std::string& r) {
+                                size_t pos = 0;
+                                while((pos = s.find(f, pos)) != std::string::npos) {
+                                    s.replace(pos, f.length(), r);
+                                    pos += r.length();
+                                }
+                            };
+                            replaceAll(targetUrl, "%3A", ":");
+                            replaceAll(targetUrl, "%2F", "/");
+
+                            std::cout << "[WebViss Gateway] Intercepted browser request for VTS URL: " << targetUrl << "\n";
+                            
+                            webviss::protocol("vts");
+                            std::string decryptedHtml = webviss::fetch(targetUrl);
+
+                            std::string httpRes = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: " + std::to_string(decryptedHtml.length()) + "\r\nConnection: close\r\n\r\n" + decryptedHtml;
+                            send(client, httpRes.c_str(), (int)httpRes.length(), 0);
+                        } else {
+                            std::string defaultHtml = "<html><body><h1>WebViss Gateway is running!</h1><p>Type <code>vts://host</code> in Windows Run or browser to browse securely.</p></body></html>";
+                            std::string httpRes = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: " + std::to_string(defaultHtml.length()) + "\r\nConnection: close\r\n\r\n" + defaultHtml;
+                            send(client, httpRes.c_str(), (int)httpRes.length(), 0);
+                        }
+                    }
+                    closesocket(client);
+                }
+            }).detach();
+        }
     }
 
     namespace env {
